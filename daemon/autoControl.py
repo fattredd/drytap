d4fb6498d3b3d6f6c71bb101750f2aa5de51183d
Jack Butler <fattredd@gmail.com>
2017-11-14 13:18:30 -0500

    Working on autoboot process. /etc/init.d/ isn't working right

--- daemon/autoControl.py
+++ daemon/autoControl.py
@@ -8,7 +8,17 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 class AutoTap(Daemon):
+        def stop(self):
+                daemonActivePin = 8
+                gpio.setwarnings(False)
+                gpio.setmode(gpio.BOARD)
+                gpio.setup(daemonActivePin , gpio.OUT)
+                gpio.output(daemonActivePin, gpio.LOW)
+                super(AutoTap, self).stop()
+        
 	def run(self):
+                daemonActivePin = 8
+                
                 log_formatter = logging.Formatter('%(message)s')
                 logFile = '/tmp/auto.log'
                 handler = RotatingFileHandler(logFile, mode='a', maxBytes=5*1024*1024,
@@ -16,6 +26,11 @@ class AutoTap(Daemon):
                 handler.setFormatter(log_formatter)
                 log = logging.getLogger('root')
                 log.addHandler(handler)
+
+                gpio.setwarnings(False)
+                gpio.setmode(gpio.BOARD)
+                gpio.setup(daemonActivePin , gpio.OUT)
+                gpio.output(daemonActivePin, gpio.HIGH)
                 
                 while True:
                         with sql.connect('/var/www/data/autoOn.db') as con:
@@ -46,21 +61,21 @@ class AutoTap(Daemon):
                                         totalStates = cur.fetchone()[0]
                                         nextState = (current+1)%totalStates
                                         cur.execute("UPDATE Step SET current = %s" % nextState)
-                                gpio.setwarnings(False)
-                                gpio.setmode(gpio.BOARD)
-                                gpio.setup(pins, gpio.OUT)
-                                log.info("Now in state %s.\n" % current)
-                                for pin, state in pinstate:
-                                        gpio.output(pin, state)
-                                        log.info("\t%s | %s\n" % (pin,state))
-                                time.sleep(delay / 1000.0)
-                                log.info("  Delay for %s ms\n" % delay)
+                                if delay == -1:
+                                        running = false
+                                else:
+                                        gpio.setup(pins, gpio.OUT)
+                                        log.info("Now in state %s.\n" % current)
+                                        for pin, state in pinstate:
+                                                gpio.output(pin, state)
+                                                log.info("\t%s | %s\n" % (pin,state))
+                                                time.sleep(delay / 1000.0)
+                                                log.info("  Delay for %s ms\n" % delay)
                         else:
                                 # Do nothing
                                 time.sleep(0.5)
-                        
 
-                
+        
 if __name__ == "__main__":
 	daemon = AutoTap('/tmp/AutoTap.pid',stderr='/tmp/autoErr.log')
 	if len(sys.argv) == 2:
@@ -68,7 +83,7 @@ if __name__ == "__main__":
 			daemon.start()
 		elif 'stop' == sys.argv[1]:
 			daemon.stop()
-		elif 'restart' == sys.argv[1]:
+  		elif 'restart' == sys.argv[1]:
 			daemon.restart()
                 elif 'status' == sys.argv[1]:
                         daemon.report()
